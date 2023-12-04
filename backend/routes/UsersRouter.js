8e7d5537035b523b094f9cdeae90c26c00a5986b
tufan özkan <tuf.ozkan@gmail.com>
2023-12-05 00:45:10 +0300

    Merge branch 'main' of github.com:ChaganYangoz/fitlife

--- backend/routes/UsersRouter.js
+++ backend/routes/UsersRouter.js
@@ -39,26 +39,11 @@ router.post("/login", async (req, res) => {
 
     const user = await User.findOne({ email }); // E-postaya göre kullanıcıyı bul
 
-<<<<<<< HEAD
-    bcrypt.compare(password, hashedPassword, (err, result) => {
-      if (err) {
-        // Hata yönetimi yapılır
-      } else if (result) {
-        res.status(200).json({ message: 'Login successful', user });
-      } else {
-        if (!user || user.password !== result) {
-          return res.status(401).json({ message: 'Invalid email or password' });
-        }
-      }
-    });
-    
-=======
-    if (!user || user.password !== password) {
-      return res.status(401).json({ message: "Invalid email or password" });
-    }
+		if (!user || user.password !== password) {
+			return res.status(401).json({ message: 'Invalid email or password' });
+		}
 
-    res.status(200).json({ message: "Login successful", user });
->>>>>>> d622feaca85a28c5ab7651d0136e47e44c30dddf
+    res.status(200).json({ message: 'Login successful', user });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
