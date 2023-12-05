84eb2ca8de83d978b951d61e758980c34328ce05
tufan özkan <tuf.ozkan@gmail.com>
2023-12-05 17:53:54 +0300

    gg

--- backend/routes/UserCoachRouter.js
+++ backend/routes/UserCoachRouter.js
@@ -1,4 +1,5 @@
 const UserCoach = require("../models/UserCoach");
+const User = require("../models/User");
 var express = require('express');
 var router = express.Router();
 
@@ -14,5 +15,34 @@ router.post('/', async (req, res) => {
       res.status(500).json({ error: error.message });
     }
   });
+
+  router.get('/userData/:coachId', async (req, res) => {
+    try {
+      const coachId = req.params.coachId;
+  
+      // Koç ID'sine göre UserCoach modelindeki tüm ilgili kullanıcılar
+      const userCoaches = await UserCoach.find({ coach_id: coachId });
+  
+      if (!userCoaches || userCoaches.length === 0) {
+        return res.status(404).json({ message: 'Koça bağlı kullanıcı bulunamadı.' });
+      }
+  
+      const userIds = userCoaches.map(userCoach => userCoach.user_id);
+  
+      // UserCoach'taki user_id'lerine göre User modelinden ilgili kullanıcıları bulun
+      const usersData = await User.find({ _id: { $in: userIds } });
+  
+      if (!usersData || usersData.length === 0) {
+        return res.status(404).json({ message: 'Kullanıcılar bulunamadı.' });
+      }
+  
+      res.status(200).json({ usersData });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
   
   module.exports = router;
+
+  // bir koc kac kullanıcıya bağlı
+  // koca bağlı tüm userları dondur ya da id döndür
